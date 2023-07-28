2dfd8fa6798465a5a7cda44882e1b3627262f2eb
Dominik Szopa <dszopa@rtsd26.org>
2023-07-28 12:18:14 -0500

    Add btn alignment for bigger screens
    This commit makes it so that buttons are properly aligned on different
    screen sizes

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ function displayBtn() {
   var text = document.getElementById("info-btns");
   isClick = !isClick;
 
-  if(isClick)
+  if(isClick && mediaQ.matches)
     text.style.display = "block";
-  else
-    text.style.display = "none";
-
-  if(isClick && mediaQuery.matches)
+  else if(isClick && mediaQuery.matches)
     text.style.display = "inline";
   else
     text.style.display = "none";
 }
 
+const mediaQ = window.matchMedia('(min-width: 1100px)')
 const mediaQuery = window.matchMedia('(max-width: 1100px)'); // Adjust the max-width to your desired breakpoint
 handleDisplayType(mediaQuery); // Call the function once to apply initial styles
-mediaQuery.addListener(handleDisplayType); // Add listener for changes in screen size
