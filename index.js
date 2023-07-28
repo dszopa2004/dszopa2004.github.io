bf7b257d9e2d8ef60b2b46480d74ea2b230efed4
Dominik Szopa <dszopa@rtsd26.org>
2023-07-28 10:59:14 -0500

    Add functionality to change btns on screen size
    This commit allows the buttons to be displayed horizontally if the
    screen size isn't big enough. Currently, the buttons do NOT display
    vertically at all.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var isClick = false;
 function displayBtn() {
   var text = document.getElementById("info-btns");
@@ -7,4 +8,13 @@ function displayBtn() {
     text.style.display = "block";
   else
     text.style.display = "none";
+
+  if(isClick && mediaQuery.matches)
+    text.style.display = "inline";
+  else
+    text.style.display = "none";
 }
+
+const mediaQuery = window.matchMedia('(max-width: 1100px)'); // Adjust the max-width to your desired breakpoint
+handleDisplayType(mediaQuery); // Call the function once to apply initial styles
+mediaQuery.addListener(handleDisplayType); // Add listener for changes in screen size
